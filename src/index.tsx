c46608f8245fc4257382142426e0256397d72507
Lisla Fox <lisla@Lislas-MBP.home>
2022-09-28 16:37:23 +0300

    Fixes+gh-pages

--- src/index.tsx
+++ src/index.tsx
@@ -5,15 +5,16 @@ import reportWebVitals from './reportWebVitals';
 import App from './app/App';
 import {Provider} from 'react-redux';
 import {store} from './app/store';
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, HashRouter} from "react-router-dom";
 
 ReactDOM.render(
     <React.StrictMode>
         <Provider store={store}>
-            <BrowserRouter>
+            <HashRouter>
+                {/*<BrowserRouter>*/}
                 <App/>
-            </BrowserRouter>
-
+                {/*</BrowserRouter>*/}
+            </HashRouter>
         </Provider>
     </React.StrictMode>,
     document.getElementById('root')
